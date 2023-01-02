d95a7882dc79b6ac46183104de9fae48e76895cd
carraes <carraeshb@gmail.com>
2023-01-02 11:53:16 -0300

    feat: write and read file

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 func main() {
-  cards := newDeck()
+	cards := newDeckFromFile("my_cards")
 
-  hand, remainingCards := deal(cards, 5)
-
-  hand.print()
-  remainingCards.print()
+	cards.print()
 }
-
